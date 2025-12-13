d3f073e25d2e9555cbdfa34b56c296e678859d11
happyjunwon20 <happyjunwon20@gmail.com>
2025-12-13 21:57:47 +0900

    Add files via upload

--- reminder.py
+++ reminder.py
@@ -12,25 +12,29 @@ st.title("리마인더")
 st.write("오늘 할 일을 관리해 봅시다")
 
 if "todolist" not in st.session_state:
-    st.session_state.todolist = {}
+    st.session_state.todolist = []
     
 if "isdone" not in st.session_state:
     st.session_state.isdone = []
 
 if "Username" not in st.session_state:
-    st.session_state.Username = ""
+    st.session_state.Username = []
+
+if "nowname" not in st.session_state:
+    st.session_state.nowname = ""
 
 st.header("이름")
 nameinput = st.text_input("이름을 입력하세요")
 if st.button("완료"):
-    st.session_state.Username = nameinput
-    st.session_state.todolist[st.session_state.Username] = []
+    st.session_state.nowname = nameinput
+    st.session_state.Username.append(st.session_state.nowname)
+    st.session_state.todolist.append([])
     try:
-        with open(rf"C:\Users\주원\Desktop\난 최고여\사람들의 리마인더\{st.session_state.Username}.txt", "r", encoding = "utf-8") as f:
+        with open(rf"C:\Users\주원\Desktop\난 최고여\사람들의 리마인더\{st.session_state.nowname}.txt", "r", encoding = "utf-8") as f:
             for lin in f:
-                st.session_state.todolist[st.session_state.Username].append(lin.rstrip())
+                st.session_state.todolist[st.session_state.Username.index(st.session_state.nowname)].append(lin.rstrip())
     except:
-        st.session_state.todolist[st.session_state.Username] = []
+        st.session_state.todolist[st.session_state.Username.index(st.session_state.nowname)] = []
 
 st.header("새 할일 추가")
 
@@ -39,8 +43,8 @@ new_todo = st.text_input("해야 할 일을 입력하세요", placeholder = "친
 if st.button("할 일 추가하기"):
     if st.session_state.Username:
         if new_todo:
-            st.session_state.todolist[st.session_state.Username].append(new_todo)
-            with open(rf"C:\Users\주원\Desktop\난 최고여\사람들의 리마인더\{st.session_state.Username}.txt", "w", encoding = "utf-8") as f:
+            st.session_state.todolist[st.session_state.Username.index(st.session_state.nowname)].append(new_todo)
+            with open(rf"C:\Users\주원\Desktop\난 최고여\사람들의 리마인더\{st.session_state.nowname}.txt", "w", encoding = "utf-8") as f:
                 f.write(new_todo + "\n")
             st.info(f"{new_todo} 할 일이 추가되었습니다")
             st.rerun()
@@ -52,20 +56,19 @@ if st.button("할 일 추가하기"):
 st.header("할 일 확인")
 try:
     cols = st.columns(len(st.session_state.todolist))
-    nameofuser = list(st.session_state.todolist.keys())
     for i, thiscol in enumerate(cols):
         with thiscol:
-            st.write(f"{nameofuser[i]}님의 할 일")
-            if st.session_state.todolist[st.session_state.Username]:
-                st.write(f"총 {len(st.session_state.todolist[st.session_state.Username])}개의 할 일이 있어요!")
+            st.write(f"{st.session_state.Username[i]}님의 할 일")
+            if st.session_state.todolist[i]:
+                st.write(f"총 {len(st.session_state.todolist[i])}개의 할 일이 있어요!")
     
-                for i, todo in enumerate(st.session_state.todolist[st.session_state.Username]):
-                    st.write(f"{i + 1}번째 할 일 : {todo}")
+                for j, todo in enumerate(st.session_state.todolist[i]):
+                    st.write(f"{j + 1}번째 할 일 : {todo}")
             else:
                 st.info("아직 할 일이 없어요. ")
 except:
     st.info("아직 아무도 없어요")
 if st.button("전체 삭제"):
-    st.session_state.todolist = {}
+    st.session_state.todolist[st.session_state.Username.index(st.session_state.nowname)] = []
     st.info("모든 할 일이 삭제되었습니다")
     st.rerun()
